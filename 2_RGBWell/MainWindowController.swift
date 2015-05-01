fb61f8ae112eaca17d094ad500391b5e7da3f9a9
Hidenori TAKESHITA <taketin@linecorp.com>
2015-05-01 17:10:09 +0900

    Adding sample app for section2

---
+++ 2_RGBWell/MainWindowController.swift
@@ -0,0 +1,13 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Takeshita Hidenori on 2015/05/01.
+//  Copyright (c) 2015年 taketin. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+}
